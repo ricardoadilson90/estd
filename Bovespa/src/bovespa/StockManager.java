6c1f5f920d7adef1c5bccf1873edd9a27fff949c
Ricardo Silva <ricardo.adilson@univille.br>
2017-03-16 22:03:10 -0300

    aula 5

--- Bovespa/src/bovespa/StockManager.java
+++ Bovespa/src/bovespa/StockManager.java
@@ -4,13 +4,14 @@ package bovespa;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 public class StockManager {
 
 	public Stock[] loadStocks(String fileName) throws IOException{
 		
-			Files.lines(Paths.get(fileName));
-		
+			Stream<String> lines = Files.lines(Paths.get(fileName));
+			lines.forEach(string -> System.out.println(string));
 		
 		
 		return null;
