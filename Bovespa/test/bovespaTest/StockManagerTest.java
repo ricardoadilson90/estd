e065ac63dd231254f2401e1916a677a7f783e4f2
Ricardo Silva <ricardo.adilson@univille.br>
2017-03-20 22:07:22 -0300

    aula 5

--- Bovespa/test/bovespaTest/StockManagerTest.java
+++ Bovespa/test/bovespaTest/StockManagerTest.java
@@ -13,6 +13,7 @@ public class StockManagerTest {
 	public void loadStocksTest() throws IOException{
 		StockManager stockManager = new StockManager();
 		Stock[] stocks = stockManager.loadStocks("Z:/3 ano/COTAHIST_A2016.txt");
+		for(Stock stock : stocks)
 		if (stock.getCode().equalsIgnoreCase("PETR4") && stock.getDate().getMonthValue() == 3
 				&& stock.getDate().getDayOfMonth() == 21 && stock.getDate().getYear() == 2016) {
 			System.out.println("Codigo\tdata\topen Price");
